36ff7e75f2bde24df149f890628d0e0142dada41
SharanStudio <141206291+SharanStudio@users.noreply.github.com>
2026-09-16 22:11:26 +0530

    Update generate-abstract.ts

--- api/generate-abstract.ts
+++ api/generate-abstract.ts
@@ -1,14 +1,3 @@
-export default async function handler(req, res) {
-  // CORS headers
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-  // Handle OPTIONS request
-  if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
-  }
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import { claudeClient, MODEL, MAX_TOKENS } from '../utils/claudeClient';
 import { buildGenerationPrompt, AbstractMetadata } from '../utils/promptEngine';
@@ -39,14 +28,12 @@ interface GeneratedArtifact {
 }
 
 export default async (req: VercelRequest, res: VercelResponse) => {
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  // CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
+  // Handle OPTIONS request
   if (req.method === 'OPTIONS') {
     res.status(200).end();
     return;
@@ -126,4 +113,3 @@ export default async (req: VercelRequest, res: VercelResponse) => {
     });
   }
 };
-}
